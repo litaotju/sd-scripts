4315f6e2155e2ea919ebca36c43eb015be23cacc
Li Tao <litaotju@live.cn>
2023-03-13 23:15:49 +0800

    Add v1.0.1, which seems works

--- scripts/train_mkl_v1.0.sh
+++ scripts/train_mkl_v1.0.sh
@@ -1,11 +1,16 @@
+set -x
+
 ## Change this for your model's data and model name, and number of steps
-export input=./data/mkl.v0.9
-export model_name="mkl.v0.9.2"
-export resume_model="mkl.v0.9.1"
-export resume_from="output/${resume_model}/${resume_model}-state"
-export steps=2000
-export epochs=100
-export prompt=gen.txt
+input="./data/mkl.v1.0"
+model_name="mkl.v1.0.1"
+resume_model="mkl.v1.0.1"
+resume_from="output/${resume_model}/${resume_model}-state"
+
+steps=2000
+epochs=100
+
+resolution="512,512"
+training_data_dir="data/mkl.v1.0/instances_512x512x100.cap/1_mkl"
 base_model=/home/litao/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
 
 ######################## almost common across all different models ##########################
@@ -24,8 +29,8 @@ accelerate launch --num_cpu_threads_per_process 1 \
     --pretrained_model_name_or_path=${base_model} \
     --mixed_precision=fp16 \
     --train_batch_size=1 \
-    --unet_lr=5e-5 \
-    --text_encoder_lr=1e-5 \
+    --unet_lr=2e-4 \
+    --text_encoder_lr=5e-5 \
     --optimizer_type=AdamW8bit \
     --network_module=networks.lora \
     --save_model_as=safetensors  \
@@ -38,24 +43,25 @@ accelerate launch --num_cpu_threads_per_process 1 \
     --save_last_n_epochs_state 1 \
     --save_state  \
     \
-    --resolution=256,256 --random_crop --enable_bucket --color_aug \
+    --resolution=${resolution} --color_aug \
     \
     --sample_prompts="${input}/prompts.txt" \
-    --train_data_dir="${input}/instances/1_mkl" \
+    --train_data_dir=${training_data_dir} \
+    --in_json=${input}/meta.json \
     ${REG_DATA_OPTION} \
     --output_name="${model_name}" \
     --output_dir=./output/${model_name} \
     --logging_dir=./output/${model_name} \
     --xformers \
-    --caption_extension=.txt \
-    --in_json=data/mkl.v0.9/meta.json \
     ${RESUME_OPTION}
 
 cp ./output/${model_name}/${model_name}.safetensors  ~/stable-diffusion-webui/models/Lora
 
 for strength in 0.0 0.2 0.4 0.6 0.8 1.0; do
-    python ./gen_img_diffusers.py --from_file $prompt  --images_per_prompt 1 \
+    python ./gen_img_diffusers.py --from_file ${input}/prompts.txt --images_per_prompt 1 \
     --outdir output/${model_name}/generated/ --fp16 --xformers --batch_size 6 \
     --network_module networks.lora --network_weights output/${model_name}/${model_name}${epoch}.safetensors  --network_mul ${strength} \
-    --ckpt ${base_model} --steps 30 --H 768 --W 512 --seed 0
+    --ckpt ${base_model} --steps 30 --H 600 --W 400 --seed 0
 done
+
+set +x
